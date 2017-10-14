1e78e70ef02667a89ad07545d58bb6849adb7a31
DESKTOP-S8BPKT5\Kyanpai <jerome.calmat@gmail.com>
2017-10-14 13:56:22 +0200

    Beginning of gesture

---
+++ Assets/GamesCreated/TheGrandFinale/Scripts/GestureEventManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ESwipeType {
+	HorizontalSwipe,
+	VerticlaSwipe,
+	RoundSwipe
+}
+
+public class OnSwipeEvent : GameEvent {
+	public Collider2D _coll = null;
+	public ESwipeType _swipeType;
+	public EInstrument _instrument;
+}
+
+public class GestureEventManager : MonoBehaviour {
+
+	private void Update () {
+		//Handle gestures
+
+		/*
+		 * 
+		 * 
+		 */
+
+		if (Input.GetMouseButtonDown(0)) {
+			RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
+			if (hit.collider != null) {
+//				Events.Instance.Raise(new OnSwipeEvent() { _swipeType = ESwipeType.VerticlaSwipe, _coll = hit.collider, _instrument = hit.collider.gameObject.GetComponent<Instrument>() });
+			}
+		}
+	}
+}
