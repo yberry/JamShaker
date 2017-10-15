7e57bef55da6c1eb963c0fb8e7da287d8f7f7359
DESKTOP-RFPEN5H\Paulo <paulcmoi51@gmail.com>
2017-10-15 08:02:16 +0200

    Adding voice feedback

---
+++ Assets/GamesCreated/TheGrandFinale/Scripts/Effect/AutoRotate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotate : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.forward, 100 * Time.deltaTime);
+    }
+}
