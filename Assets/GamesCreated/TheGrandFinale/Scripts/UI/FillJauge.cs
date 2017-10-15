ec0f13fb5409e7c88e66c9807e5595e102232a1d
yberry <yanisberry16@gmail.com>
2017-10-15 13:45:23 +0200

    FillJauge

---
+++ Assets/GamesCreated/TheGrandFinale/Scripts/UI/FillJauge.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FillJauge : MonoBehaviour {
+
+    private static FillJauge instance = null;
+    public static FillJauge Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    public Image fillImage;
+    public float fillSpeed = 2f;
+
+    private float scoreMax = 1f;
+    private float currentScore = 0f;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetMax(float max)
+    {
+        scoreMax = max;
+    }
+
+    public void SetCurrentScore(float score)
+    {
+        currentScore = score;
+    }
+
+    void Update()
+    {
+        fillImage.fillAmount = Mathf.MoveTowards(fillImage.fillAmount, currentScore / scoreMax, fillSpeed * Time.deltaTime);
+    }
+}
